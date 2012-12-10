30f3ede2914988e18b15d6d92280f68a019bfc97
ian mcgaunn <ianmcgaunn@gmail.com>
2012-12-10 14:35:14 -0500

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ johnstamos
 ==========
 
 a basic JS applet for redirecting login forms with some uncle jesse flavor.
+I'm not sure anything else needs to be said.
---
+++ johnstamos-0.1.js
@@ -0,0 +1,74 @@
+//Copyright 2012 Ian McGaunn
+//code adapted from work by Alex Sirota
+
+(function() {
+
+    function getInputs(form) {
+
+        var inputs = form.getElementsByTagName('input');
+        var data = [];
+
+        for (var i = 0; i < inputs.length; i++) {
+
+            var input = inputs[i];
+            var type = input.getAttribute('type');
+
+            if ((type == 'text') || (type == 'password')) {
+
+                var name = input.getAttribute('name');
+                data.push(name + ' = ' + input.value);
+
+            }
+        }
+
+        if (data.length > 0) alert(data) ;
+
+    }
+
+    function onFormSubmit(event) {
+
+        var target;
+
+        // allow for terrible microsoft event accessing model
+        if (!event) var event = window.event;
+
+        if (event.target) target = event.target;
+        else if (event.srcElement) target = event.srcElement;
+        if (!target) return;
+
+        getInputs(target);
+
+    }
+
+    function stamosize() {
+
+        //intravenous stamosization.
+        var bannerdiv = document.createElement("div");
+        var bannerimg = document.createElement("img");
+        var divholder = document.getElementsByTagName("body")[0];
+        var eldest = divholder.childNodes[0];
+
+        bannerdiv.setAttribute("style", "text-align: center; width: 100%; background: #000;");
+        bannerdiv.setAttribute("style", "font-family: Helvetica; font-size: 4em; " )
+        bannerdiv.appendChild(bannerimg);
+        divholder.insertBefore(bannerdiv, eldest);
+
+    }
+
+    stamosize() ;
+    var forms = document.getElementsByTagName('form');
+
+    for (var i = 0; i < forms.length; i++) {
+
+        var form = forms[i];
+
+        if (form.addEventListener) {
+            form.addEventListener('submit', onFormSubmit, true);
+        }
+
+        else if (form.attachEvent) {
+            form.attachEvent('onsubmit', onFormSubmit);
+        }
+    }
+
+})();
